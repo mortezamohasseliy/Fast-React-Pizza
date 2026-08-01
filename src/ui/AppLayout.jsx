c43db811f4199a4cc758a68e52c19a91749ea6f8
mortezamohasseliy <murteza.mohasseliy@gmail.com>
2026-08-01 22:29:45 +0430

    create and styling the app with tailwindcss

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,27 @@
+import Header from './Header';
+import CartOverView from '../features/cart/CartOverview';
+import { Outlet, useNavigation } from 'react-router-dom';
+import Loader from './Loader';
+
+function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading';
+
+  return (
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {isLoading && <Loader />}
+      <Header />
+
+      <div className="my-10 overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <h1>Content</h1>
+          <Outlet />
+        </main>
+      </div>
+
+      <CartOverView />
+    </div>
+  );
+}
+
+export default AppLayout;
