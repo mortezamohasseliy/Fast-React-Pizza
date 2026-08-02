6ce97cf898485a27bb36051dd97461c39811a1c2
mortezamohasseliy <murteza.mohasseliy@gmail.com>
2026-08-02 23:55:12 +0430

    add roboto fonts and design menu with reusable button components to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: 'Roboto Mono, monospace',
+    },
+
+    extend: {
+      fontSize: {
+        huge: ['80rem', { lineHeight: '1' }],
+      },
+      height: {
+        screen: '100dvh',
+      },
+    },
   },
   plugins: [],
 };
